4f86a8c29d56cefa571a407ec3bfc782c22ccdc5
TheNear <tomtom.leman@gmail.com>
2025-11-14 12:42:31 +0100

    darkmode :'class'

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  darkMode: 'class', // enables dark mode via the "dark" class
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}", // point to your source files
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
