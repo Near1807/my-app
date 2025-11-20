f9147e57256b3ea1ea5affce545941672b177eea
TheNear <tomtom.leman@gmail.com>
2025-11-20 16:42:26 +0100

    blog visual

--- app/Blog/page.tsx
+++ app/Blog/page.tsx
@@ -8,8 +8,8 @@ export default async function Blog() {
     <div className="bg-white dark:bg-gray-900 min-h-screen transition-colors duration-200">
       <form action={SendMessage} className="text-center p-5">
         <Text className="">What's your message my dear</Text>
-        <input className="border" name="message" />
-        <button className="border ml-1 px-1 rounded-2xl hover:bg-gray-200">
+        <input className="border border-gray-300" name="message" />
+        <button className="border border-gray-300 ml-1 px-1 rounded-2xl hover:bg-gray-200">
           Submit
         </button>
       </form>
@@ -20,9 +20,9 @@ export default async function Blog() {
             className="flex flex-row grow"
             action={RemoveElem.bind(null, id)}
           >
-            <div className="grid grid-cols-5 grow p-5 border rounded-2xl dark:bg-gradient-to-br from-gray-900 to-gray-800">
+            <div className="grid grid-cols-5 grow p-5 border border-gray-300 rounded-2xl dark:bg-gradient-to-br from-gray-900 to-gray-800">
               <div className="col-span-4">{elem.message}</div>
-              <button className="hover:bg-gray-200 border col-span-1">
+              <button className="bg-sky-100 hover:bg-sky-200 border border-gray-300 col-span-1 ">
                 delete
               </button>
             </div>
